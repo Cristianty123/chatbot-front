fb469a8034c96fae004b3186cbe7adc7452ceaac
aAguilarDaniel <aaguilardaniel8@gmail.com>
2025-08-20 23:51:54 -0500

    FrontEnd: se agregó cursor, fuente, rutas, interfaz chatbot y fix de imágenes en la página de inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './page/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'Home', pathMatch: 'full'},
+    {path: 'Home', component: Home},
+];
+
