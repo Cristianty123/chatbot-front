ff3e42bccf6a8189d9814f18ac5e6ee88e70f620
Cristian Gomez <cristianty123@gmail.com>
2025-08-21 22:17:29 -0500

    se arreglo el responsive de register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { Home } from './page/home/home';
 import {Register} from './page/register/register';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'Home', pathMatch: 'full'},
-    {path: 'Home', component: Home, data: {animation: 'HomePage'}},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: Home, data: {animation: 'HomePage'}},
     {path: 'register', component: Register, data: {animation: 'RegisterPage'}},
 ];
 
--- src/app/page/register/register.ts
+++ src/app/page/register/register.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './register.css'
 })
 export class Register implements OnInit {
+
+  constructor(private router: Router) {}
   ngOnInit() {
     document.documentElement.setAttribute('data-theme', 'login');
+  }
+
+  navigateToHome() {
+    this.router.navigate(['/']).then(() => {
+      // Navegación completada con éxito
+      console.log('Navegación a registro completada');
+    }).catch((error) => {
+      // Manejo de errores si la navegación falla
+      console.error('Error en navegación:', error);
+    });
   }
 }
